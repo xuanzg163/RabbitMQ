313ec87be5db34ebf231aa28f48ee7846a58223e
xuanzg163 <xuan_zg@163.com>
2018-11-23 11:21:01 +0800

    RabbitMQ学习使用

---
+++ src/main/java/test/RabbitMQ/Receiver.java
@@ -0,0 +1,11 @@
+package test.RabbitMQ;
+
+/**
+ * 消息接收方
+ * @author zhangxuan
+ * @date 2018/11/23
+ * @time 11:18
+ */
+
+public class Receiver {
+}
---
+++ src/main/java/test/RabbitMQ/Send.java
@@ -0,0 +1,11 @@
+package test.RabbitMQ;
+
+/**
+ * 消息发送方
+ * @author zhangxuan
+ * @date 2018/11/23
+ * @time 11:17
+ */
+
+public class Send {
+}
